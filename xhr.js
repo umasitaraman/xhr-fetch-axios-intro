f2d7ed778b998bf483d2309643efc999076e8dc2
umasitaraman <36041143+umasitaraman@users.noreply.github.com>
2022-04-06 11:01:31 -0400

    Followed along the Youtube video to complete the implementation

--- xhr.js
+++ xhr.js
@@ -1,49 +1,48 @@
 const getBtn = document.getElementById('get-btn');
 const postBtn = document.getElementById('post-btn');
 
-const sendHttpRequest = (method, url, data) => {
-  const promise = new Promise((resolve, reject) => {
-    const xhr = new XMLHttpRequest();
-    xhr.open(method, url);
+ const sendHttpRequest = (method, url, data) => {
+     const promise = new Promise((resolve, reject) => {
+        const xhr = new XMLHttpRequest();    // Global constructor function built into a browser exposed to Java Script
+        xhr.open(method, url);
 
-    xhr.responseType = 'json';
+        xhr.responseType = 'json';
+        if(data) {
+            xhr.setRequestHeader('Content-Type', 'application/json');    
+        }
 
-    if (data) {
-      xhr.setRequestHeader('Content-Type', 'application/json');
-    }
+        xhr.onload = () => {
+            // const data = JSON.parse(xhr.response);
+            // const data = xhr.response;
+            // console.log(data); 
+            if(xhr.status >= 400) {
+              reject(xhr.response);
+            } else {
+              resolve(xhr.response);
+            }
+        };
 
-    xhr.onload = () => {
-      if (xhr.status >= 400) {
-        reject(xhr.response);
-      } else {
-        resolve(xhr.response);
-      }
-    };
+        xhr.onerror = () => {
+          reject('Something went wrong!');
+        };
 
-    xhr.onerror = () => {
-      reject('Something went wrong!');
-    };
-
-    xhr.send(JSON.stringify(data));
-  });
-  return promise;
-};
-
+        xhr.send(JSON.stringify(data));   
+    });
+    return promise;    
+ };
 const getData = () => {
-  sendHttpRequest('GET', 'https://reqres.in/api/users').then(responseData => {
-    console.log(responseData);
-  });
+    sendHttpRequest('GET', 'https://reqres.in/api/users').then(resposeData => {
+        console.log(resposeData);
+    });
 };
 
 const sendData = () => {
-  sendHttpRequest('POST', 'https://reqres.in/api/register', {
-    email: 'eve.holt@reqres.in'
-    // password: 'pistol'
-  })
-    .then(responseData => {
-      console.log(responseData);
-    })
-    .catch(err => {
+    sendHttpRequest('POST', 'https://reqres.in/api/register', {
+      email: "eve.holt@reqres.in",
+      password: "pistol"
+    }).then(resposeData => {
+        console.log(resposeData);
+    }).catch(err => {
       console.log(err);
     });
 };
